5d843823cae7bb57cd25aecd2ca11cd2fef10bef
KojoHero <60008545+KojoHero@users.noreply.github.com>
2021-01-25 14:37:47 +0000

    slight update

--- Auth/serializers.py
+++ Auth/serializers.py
@@ -8,8 +8,8 @@ from .models import Registration, BlogPost, Category, TodoList
 
 
 class RegSerializers(serializers.ModelSerializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=False, validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(required=False)
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     c_password = serializers.CharField(write_only=True, required=True)
 
@@ -24,7 +24,7 @@ class RegSerializers(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(username=validated_data['username'], email=validated_data['email'])
+        user = User.objects.create(email=validated_data['email'], username=validated_data['username'] )
         user.set_password(validated_data['password'])
         user.save()
 
@@ -35,7 +35,7 @@ class LogSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Registration
-        fields = ('username', 'password')
+        fields = ('email', 'password')
 
 
 class BlogSerializer(serializers.ModelSerializer):
